d8d84ecdb69192168aad45a5b48356ace8700291
welik09 <welikcarlosluiz@gmail.com>
2024-01-25 21:12:07 -0300

    commit

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-document.getElementById("navbar").HTMLElementObject.blur('')
+var span = document.querySelectorAll('.texto span')[0];
+
+span.addEventListener('click',function(){
+    var texto = document.querySelectorAll('.texto p')[0];
+
+    if(texto.classList.contains('mostrar')){
+        span.innerHTML = 'ver mais';
+        texto.classList.remove('mostrar');
+
+    }else{
+        span.innerHTML = "ocultar";
+        texto.classList.add('mostrar');
+    }
+
+});
+
+var span = document.querySelectorAll('.texto span')[1];
+
+span.addEventListener('click',function(){
+    var texto = document.querySelectorAll('.texto p')[1];
+
+    if(texto.classList.contains('mostrar')){
+        span.innerHTML = 'ver mais';
+        texto.classList.remove('mostrar');
+
+    }else{
+        span.innerHTML = "ocultar";
+        texto.classList.add('mostrar');
+    }
+
+});
